4ff1737c614b8de4b4d099d9d1e5a17ba72e71ae
Rezaul Karim <rezaul@orangetoolz.com>
2025-05-18 13:40:28 +0600

    add

--- web/src/assets/types.ts
+++
@@ -1,6 +0,0 @@
-export interface IIconProps {
-    size?:number
-    color?:string
-    className?:string
-    onClick?:()=>void
-}
--- web/src/constance/secret.ts
+++
@@ -1,1 +0,0 @@
-export const BASE_URL = 'http://192.168.68.113:8000'
